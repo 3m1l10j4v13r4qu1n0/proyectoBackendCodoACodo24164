4e425287744c529c1dcef96a0afa509e7e1bce2a
Lucas Acosta <alucase@gmail.com>
2024-07-02 09:17:44 -0300

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ app = Flask(__name__)
 
 # Importar las vistas
 from componentes.vistas_api import *
+from componentes.vistas_api_productos import *
 from componentes.vistas_web import *
 
 # Lo siguiente sólo en desarrollo, no en producción
