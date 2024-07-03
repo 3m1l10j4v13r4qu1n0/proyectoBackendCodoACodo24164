ed72818c9a8f95b15da50600cc28e4d34c307e46
Lucas Acosta <alucase@gmail.com>
2024-07-02 23:47:22 -0300

    Lucas - se agregan cambios de los CRUD para productos

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from flask import Flask
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 # Importar las vistas
-#from componentes.vistas_api import *
+# from componentes.vistas_api import *
 from componentes.vistas_web import *
+from componentes.vistas_api_productos import *
 
 # Lo siguiente sólo en desarrollo, no en producción
 if __name__ == '__main__':
